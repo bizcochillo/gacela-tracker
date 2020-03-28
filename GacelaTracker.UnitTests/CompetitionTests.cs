44054ead83cd65c8ffa5fcff73b336288e72ad8e
bizcochillo <abelardop@gmail.com>
2020-03-28 20:11:14 +0100

    Add competition logic and tests

---
+++ GacelaTracker.UnitTests/CompetitionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GacelaTracker.UnitTests
+{
+    public class CompetitionTests
+    {
+        [Fact]
+        public void GivenNoCompetition_WhenCreated_NameIsSet()
+        {
+            // Given
+
+            // When
+            var comp = new Competition("TestTitle");
+
+            // Then
+            Assert.Equal("TestTitle", comp.Title);
+        }
+
+        [Fact]
+        public void GivenCompetition_WhenUserIsAdded_ThenUserIsInCompetition()
+        {
+            // Given
+            var player = new Player("PlayerName");
+            var comp = new Competition("CompName");
+
+            // When 
+            comp.AddPlayer(player);
+
+            // Then
+            Assert.True(comp.HasPlayer(player.Guid));
+        }
+    }
+}
---
+++ GacelaTracker/Competition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GacelaTracker
+{
+    public class Competition
+    {
+        public string Title { get; private set; }
+        private Dictionary<Guid, Player> _players;
+
+        public Competition(string title)
+        {
+            Title = title;
+            _players = new Dictionary<Guid, Player>();
+        }
+
+
+        public void AddPlayer(Player player)
+        {
+            if (_players.ContainsKey(player.Guid))
+                throw new ArgumentException($"Player {player.Name} with GUID {player.Guid} has already been added");
+            _players.Add(player.Guid, player);
+        }
+
+        public bool HasPlayer(Guid guid)
+        {
+            return _players.ContainsKey(guid);
+        }
+    }
+}
